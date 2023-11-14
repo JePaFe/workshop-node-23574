e06910dce8c4b6e304f5654ee8c00682f203208c
Jean Paul Ferreira <jepafe@gmail.com>
2023-11-14 12:12:29 -0300

    add layouts

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+app.set("view engine", "ejs");
+app.set('views', './src/views')
+
+app.use(expressLayouts);
+app.set("layout", "layouts/layout");
 
 app.use(require("./src/routes/tareasRouter"));
 
--- src/controllers/tareasController.js
+++ src/controllers/tareasController.js
@@ -1,5 +1,5 @@
 const index = (req, res) => {
-  res.send("Tareas");
+  res.render("index");
 };
 
 module.exports = {

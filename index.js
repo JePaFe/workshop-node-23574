6dae708c2d46d7d3e706cd8b1dfe964e89f5ae90
Jean Paul Ferreira <jepafe@gmail.com>
2023-11-14 12:33:18 -0300

    add absolute path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const expressLayouts = require("express-ejs-layouts");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("view engine", "ejs");
-app.set("views", "./src/views");
+app.set("views", path.join(__dirname, "/src/views"));
 
 app.use(expressLayouts);
 app.set("layout", "layouts/layout");

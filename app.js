cfbc9b9d72f044a10ed5025e7165f8334a89df13
Jean Paul Ferreira <jepafe@gmail.com>
2023-11-14 12:21:21 -0300

    add css styles

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
-app.set('views', './src/views')
+app.set("views", "./src/views");
 
 app.use(expressLayouts);
 app.set("layout", "layouts/layout");
--- src/controllers/tareasController.js
+++ src/controllers/tareasController.js
@@ -1,5 +1,11 @@
+let tareas = [
+  { id: 1, title: "Tarea 1", completed: false },
+  { id: 2, title: "Tarea 2", completed: false },
+  { id: 3, title: "Tarea 3", completed: false },
+];
+
 const index = (req, res) => {
-  res.render("index");
+  res.render("index", { tareas });
 };
 
 module.exports = {
